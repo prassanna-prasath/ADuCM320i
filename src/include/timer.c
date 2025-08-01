cb97227c28939639ccacbcd9a8df9f4bb7c4ee9a
Prassanna-Prasath <prassannacharu@gmail.com>
2025-08-01 16:50:12 +0100

    Created library files for timer based operations.

---
+++ src/include/timer.c
@@ -0,0 +1,4 @@
+#include "../headers/pincm320i.h"
+#include "timer.h"
+
+
---
+++ src/include/timer.h
@@ -0,0 +1,19 @@
+/* Header file
+ * Description: General Purpose Timer function signatures
+ * Author: Prassanna
+ */
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+//Write your function signatures here
+
+
+
+
+
+
+
+
+
+#endif
